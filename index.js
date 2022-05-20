8e14ed06e22a928fca5a0ebda7139abd2245fd5c
unknown <coreycodes@outlook.com>
2022-05-20 14:01:39 +0100

    changed cors and added socket ports

--- index.js
+++ index.js
@@ -11,28 +11,6 @@ env.config();
 
 const totalCPUs = os.cpus().length;
 
-//Socket IO
-const httpServer = createServer();
-const io = new Server(httpServer);
-
-//Redis
-const pubClient = createClient();
-const subClient = pubClient.duplicate();
-
-Promise.all([pubClient.connect(), subClient.connect()]).then(() => {
-    io.adapter(createAdapter(pubClient, subClient));
-    io.listen(8000);
-});
-
-//Connecting to socket.io
-io.on("connection", socket => {
-    console.log(`Connection: ${socket.id}`);
-
-    socket.on('disconnect', function () {
-        console.log("disconnect: ", socket.id);
-    });
-});
-
 if (cluster.isPrimary) {
     // Fork workers.
     for (let i = 0; i < totalCPUs; i++) {
@@ -45,14 +23,43 @@ if (cluster.isPrimary) {
 } else {
     const app = express();
 
+    //Socket IO
+    const httpServer = createServer();
+    const io = new Server(httpServer, {
+        cors: {
+            origin: 'http://192.168.1.113/',
+            credentials: true,
+        }
+    });
+
+    //Redis
+    const pubClient = createClient();
+    const subClient = pubClient.duplicate();
+
+    const socketPort = process.argv[5];
+
+    Promise.all([pubClient.connect(), subClient.connect()]).then(() => {
+        io.adapter(createAdapter(pubClient, subClient));
+        io.listen(socketPort);
+    });
+
+    //Connecting to socket.io
+    io.on("connection", socket => {
+        console.log(`Connection: ${socket.id}`);
+
+        socket.on('disconnect', function () {
+            console.log("disconnect: ", socket.id);
+        });
+    });
+
     const port = process.argv[3];
 
     app.get('/', async (req, res) => {
         const value = await pubClient.get('key');
 
         const data = JSON.stringify(value);
-        return res.send(data);
-        //return res.send(`hi from port ${port}`);
+        //return res.send(data);
+        return res.send(`hi from port ${port}`);
     })
 
     app.listen(port, () => {
